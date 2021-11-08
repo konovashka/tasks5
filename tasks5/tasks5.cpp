c5c1a10ee225fce0fea940bffffc5d45ecbc34c4
konovashka <anton_konovalov_1@mail.ru>
2021-11-08 23:28:32 +0300

    Добавьте файлы проекта.

---
+++ tasks5/tasks5.cpp
@@ -0,0 +1,26 @@
+﻿// Сумма элементов, оканчивающихся пятёркой
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, x,sum;
+	scanf_s("%d", &n);
+	sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &x);
+		if (x%10==5)
+		{
+			sum += x;
+		}
+	}
+	if (sum != 0)
+	{
+		printf("%d", sum);
+	}
+	else
+	{
+		printf("chisel net");
+	}
+}
